7f5a7e54a6b12e95b1ef74679c2d9e5b6d60577b
abdheshkumar <abdhesh@callhandling.co.uk>
2018-10-16 13:29:42 +0100

    Re-produce typesafe config issue

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,5 @@ import com.typesafe.config.ConfigFactory
 
 object Main extends App {
   val c = ConfigFactory.load()
-  println("::::::" + c.getString("test.name"))
-  Thread.sleep(50000)
+  println("::::::" + c.toString)
 }
